1937c618a54d4cf796f87539896c9521778a127e
Unlawful Productions <joseph.moreno.01@gmail.com>
2016-03-20 15:01:17 -0600

    Removed Whitespace

--- RoadsUnited_Core/RoadsUnited_Core/Hook/RedirectCallsState.cs
+++ RoadsUnited_Core/RoadsUnited_Core/Hook/RedirectCallsState.cs
@@ -1,19 +1,14 @@
-﻿using System;
+using System;
 
 namespace RoadsUnited_Core
 {
     public struct RedirectCallsState
     {
         public byte a;
-
         public byte b;
-
         public byte c;
-
         public byte d;
-
         public byte e;
-
         public ulong f;
     }
 }
